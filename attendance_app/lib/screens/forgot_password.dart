687d78d8addef7c9405c8f43a8c0e982f63f270b
Yashraj2311 <yashrajsingh231105@gmail.com>
2024-09-08 12:48:25 +0530

    Initial commit

---
+++ attendance_app/lib/screens/forgot_password.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgetPasswordScreenState();
+}
+
+class _ForgetPasswordScreenState extends State<ForgotPasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Forget Password');
+  }
+}
---
+++ attendance_app/lib/screens/profile_section.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+             // ignore: prefer_const_constructors
+             CircleAvatar(
+              radius: 70,
+              // ignore: prefer_const_constructors
+              backgroundImage: AssetImage('assets/images/user.jpg'),
+            ),
+            const SizedBox(height: 20),
+            itemProfile('Name', 'Yashraj Singh',CupertinoIcons.person),
+            const SizedBox(height: 10),
+            itemProfile('Phone', '000000000' , CupertinoIcons.phone),
+            const SizedBox(height: 10),
+            itemProfile(
+                'Email', 'formovies2311@gmail.com', CupertinoIcons.mail),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(15),
+                  ),
+                  child: const Text('Edit Profile')),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  itemProfile(String title, String subtitle, IconData iconData) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+                // ignore: prefer_const_constructors
+                offset: Offset(0, 5),
+                color: Colors.deepOrange.withOpacity(.2),
+                spreadRadius: 2,
+                blurRadius: 10)
+          ]),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        leading: Icon(iconData),
+        trailing: Icon(Icons.arrow_forward, color: Colors.grey.shade400),
+        tileColor: Colors.white,
+      ),
+    );
+  }
+}
